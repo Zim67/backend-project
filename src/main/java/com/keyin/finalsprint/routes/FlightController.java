b28f05e4dc17eedc83e68b088a397064fbaf7153
RamshaIshtiaque <ramshaishtiaque@gmail.com>
2024-08-06 12:36:11 -0230

    adding fuctionality for getting flights that are departing or arriving

--- src/main/java/com/keyin/finalsprint/routes/FlightController.java
+++ src/main/java/com/keyin/finalsprint/routes/FlightController.java
@@ -1,5 +1,6 @@
 package com.keyin.finalsprint.routes;
 
+import com.keyin.finalsprint.models.Airport;
 import com.keyin.finalsprint.models.Flight;
 import com.keyin.finalsprint.routes.bodies.FlightsUpdateBody;
 import com.keyin.finalsprint.services.AircraftService;
@@ -7,6 +8,7 @@ import com.keyin.finalsprint.services.AirportService;
 import com.keyin.finalsprint.services.FlightService;
 import com.keyin.finalsprint.utils.StatusCodes;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,5 +40,18 @@ public class FlightController {
     @GetMapping("flights")
     public ResponseEntity<List<Flight>> get() {
         return StatusCodes.with(service.get());
+    }
+    @GetMapping("flights/arrivals/{airportId}")
+    public ResponseEntity<List<Flight>> getArrivalsAtAirport(@PathVariable Long airportId) {
+        Airport airport = airports.findById(airportId).orElseThrow(() -> new ResourceNotFoundException("Airport not found"));
+        List<Flight> flights = service.getArrivalsAtAirport(airport);
+        return StatusCodes.with(flights);
+    }
+
+    @GetMapping("flights/departures/{airportId}")
+    public ResponseEntity<List<Flight>> getDeparturesFromAirport(@PathVariable Long airportId) {
+        Airport airport = airports.findById(airportId).orElseThrow(() -> new ResourceNotFoundException("Airport not found"));
+        List<Flight> flights = service.getDeparturesFromAirport(airport);
+        return StatusCodes.with(flights);
     }
 }
